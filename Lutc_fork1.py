878ec6f24665bdae8a43ae9dfa4b7ef72e23140c
Artem Pilkevich <fizmat39reg.bot@mail.ru>
2015-10-05 11:07:25 +0300

    update for second example

--- Lutc_fork1.py
+++ Lutc_fork1.py
@@ -1,18 +1,23 @@
-import os
+#!/usr/bin/env python3
 
-def child():
-    print("Child ", os.getpid())
-    os._exit(0)
+import os
+import time
+
+def child_counter(count):
+    for i in range(count):
+        n = 2
+        for j in range(10**6):
+           n = (n**2) % 10**20
+        print("Child id={} i={} n={}".format(os.getpid(), i, n))
 
 def parent():
-    while True:
+    for i in range(16):
         newpid = os.fork()
         if newpid == 0:
-            child()
+            child_counter(5)
+            os._exit(0)
         else:
             print("Parent ", os.getpid(), newpid)
-        if input() == "q":
-            break
 
 if __name__ == "__main__":
     parent()
